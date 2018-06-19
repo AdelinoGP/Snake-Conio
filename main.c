873e24f2ef4e5dc6906c05cdf57a95c776de33c9
Adelino Penedo <agpene@hotmail.com>
2018-06-19 10:24:02 -0300

    Otimizações de performance.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #define TAMANHO 24
+#define TAMANHOMAX (TAMANHO + 1) * (TAMANHO + 1) * 4
 #include <time.h>
 #include <windows.h>
 #include <stdio.h>
@@ -14,16 +15,22 @@ struct objeto
     int x,y;
 };
 
+
 int main()
 {
     struct objeto cobras[200];
     struct objeto ponto;
-    char mundo[TAMANHO + 1][TAMANHO + 1];
+    COORD coord;
+    char mundo[(TAMANHO)][TAMANHO + 1];
+    char buffer[(TAMANHOMAX)];
     char teclado;
+    int cx;
     int cobrasize = 0;
     int dircobra = 0;
     int i,c;
     hidecursor();
+    coord.X=0;
+    coord.Y=0;
     srand (time(NULL));
     cobras[0].x = rand() % TAMANHO;
     cobras[0].y = rand() % TAMANHO;
@@ -35,7 +42,8 @@ int main()
     }
     for(i=0; i<TAMANHO; i++)
     {
-        for (c=0; c<TAMANHO; c++)mundo[i][c] = ' ';
+        for (c=0; c<TAMANHO; c++)
+            mundo[i][c] = ' ';
     }
     while (1)
     {
@@ -49,6 +57,7 @@ int main()
             teclado = getch();
         else
             teclado = 0;
+
         switch(teclado)
         {
             case 'w':dircobra = 0;  break;
@@ -56,12 +65,14 @@ int main()
             case 'a':dircobra = 2;  break;
             case 'd':dircobra = 3;  break;
         }
-        //copia a
-        for (i = cobrasize; i != 0; i--)
+        //Copia cobra anterior para posição da cobra da frente
+        for (i = cobrasize; i > 0; i--)
         {
             cobras[i].y = cobras[i-1].y;
             cobras[i].x = cobras[i-1].x;
         }
+
+        //Move Cobra[0]
         switch(dircobra)
         {
             case 0:cobras[0].y--;   break;
@@ -69,17 +80,19 @@ int main()
             case 2:cobras[0].x--;   break;
             case 3:cobras[0].x++;   break;
         }
-        if(cobras[0].y > TAMANHO-1)cobras[0].y = 0;
-        if(cobras[0].y < 0)cobras[0].y = TAMANHO-1;
-        if(cobras[0].x > TAMANHO-1)cobras[0].x = 0;
-        if(cobras[0].x < 0)cobras[0].x = TAMANHO-1;
+
+        //Checa se fora do limite
+        if(cobras[0].y > TAMANHO-1) cobras[0].y = 0;
+        if(cobras[0].y < 0)         cobras[0].y = TAMANHO-1;
+        if(cobras[0].x > TAMANHO-1) cobras[0].x = 0;
+        if(cobras[0].x < 0)         cobras[0].x = TAMANHO-1;
+
+
         if(cobras[0].x == ponto.x && cobras[0].y == ponto.y)
         {
             ponto.x = rand() % TAMANHO;
             ponto.y = rand() % TAMANHO;
             cobrasize++;
-            cobras[cobrasize].y = 27;
-            cobras[cobrasize].x = 27;
         }
         for (i = 0; i < cobrasize; i++){
             for (c = 0; c < cobrasize; c++){
@@ -95,15 +108,18 @@ int main()
         {
             mundo[cobras[i].y][cobras[i].x] = '0';
         }
-        system("cls");
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+        cx = 0;
         for(i = 0; i < TAMANHO; i++)
         {
-            for (c = 0; c < TAMANHO; c++)
-                printf("%c ",mundo[i][c]);
-            printf("\n");
+            for (c = 0; c < TAMANHO; c++){
+                cx += snprintf(buffer + cx,TAMANHOMAX-cx,"%c ",mundo[i][c]);
+            }
+            cx += snprintf(buffer + cx,TAMANHOMAX-cx,"|\n");
         }
+        puts (buffer);
         printf("Pontos = %d  Jogo por Adelino Penedo\n",cobrasize*5);
-        delay(100);
+        delay(150);
     }
     return 0;
 }
